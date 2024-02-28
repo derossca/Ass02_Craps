9a4773a4c135682ff181d558f330f51c7051a564
derossca <derossca@mail.uc.edu>
2024-02-28 10:50:12 -0500

    made do while loop for overall game, switch method for each case of first roll of die

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,32 @@ public class Main {
         int die1 = rnd.nextInt(6) + 1;
         int die2 = rnd.nextInt(6) + 1;
         int crapsRoll = die1 + die2;
+        int point = crapsRoll;
         boolean done = false;
+        String continueYN = "";
+        String trash = "";
 
+        //do while loop that loops the whole game
+        do
+        {
+            //display roll of die together
+            System.out.println(crapsRoll);
+            switch (crapsRoll){
+                case 2,3,12:
+                    System.out.println("Craps you lose!!!");
+                    break;
+                case 7,11:
+                    System.out.println(("Natural, you win!!!"));
+                    break;
+                case 4,5,6,8,9,10:
+                    System.out.println("Do you want to roll for point");
 
+                    break;
+
+            }
+
+            System.out.println("Do you want to continue [Y/N] ");
+            continueYN = in.nextLine();
+        } while(continueYN.equalsIgnoreCase("Y"));
     }
 }
